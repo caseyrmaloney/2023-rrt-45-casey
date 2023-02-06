e97c04f00e284f05e4b68018f4a38675c2d52601
Casey Maloney <tuj57107@temple.edu>
2023-02-06 11:13:25 -0500

    2/6/23

--- Class Examples/src/com/tek/example1/FirstClass.java
+++ Class Examples/src/com/tek/example1/FirstClass.java
@@ -5,8 +5,14 @@ public class FirstClass {
 	public static void main(String[] args) { 
 		System.out.println("Hello World");
 		
+		
+		String s = "some text a \" and a \\ in it"; 
+		
 		int i = 5+5; 
 		System.out.println("Sum of i: 5 + 5 = " + i);
+		
+		//printing out the string 
+		System.out.println(s); 
 	}
 
 }
